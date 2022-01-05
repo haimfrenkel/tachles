c904cb6d71d9c7060cc88cbbe56d11bc237b617d
haimfrenkel <hf36274@gmail.com>
2022-01-05 18:43:47 +0200

    init commit

---
+++ src/main/java/com/tachles/users/models/User.java
@@ -0,0 +1,34 @@
+package com.tachles.users.models;
+
+import lombok.*;
+import javax.persistence.*;
+import java.util.*;
+
+
+@Entity
+@Table
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User extends BaseModel {
+    private String userName;
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private PersonalInformation men;
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private PersonalInformation women;
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private Role role;
+    //password
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private Address address;
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private BankAccount bankAccount;
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "user")
+    private List<Child> children;
+    private String shtibel;
+}
+
+
+
+
